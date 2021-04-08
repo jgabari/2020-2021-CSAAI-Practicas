5bee44de79fda3c774bce60a342c26dab084b05f
jgabari <javigabari@gmail.com>
2021-04-08 09:56:16 +0200

    ejercicio 2

--- P3/S6/canvas-01.js
+++ P3/S6/canvas-01.js
@@ -2,5 +2,21 @@ console.log("Ejecutando JavaScript.....");
 
 const canvas = document.getElementById("canvas");
 
+// dimensiones del canvas
 canvas.width = 640;
 canvas.height = 480;
+
+// contexto
+const ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+    // lugar y dimensiones del rectangulo
+    ctx.rect(10,10, 200,100);
+
+    // relleno del rectangulo
+    ctx.fillStyle = 'lightgrey';
+    ctx.fill();
+
+    // trazo del rectangulo
+    ctx.stroke();
+ctx.closePath();
