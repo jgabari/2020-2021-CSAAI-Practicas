a2931a9fbe0045f2fefddd6e139618448049a2c1
jgabari <javigabari@gmail.com>
2021-05-17 18:27:07 +0200

    boton start y maquina de estados

--- P3/L8/escenario.js
+++ P3/L8/escenario.js
@@ -2,6 +2,8 @@ console.log("Ejecutando JavaScript.....");
 
 const canvas = document.getElementById("canvas");
 
+const btn_start = document.getElementById("btn_start");
+
 // dimensiones del canvas
 canvas.width = 463;
 canvas.height = 640;
@@ -25,8 +27,16 @@ let velx = 6;
 let vely = -3;
 let velx1 = 20;
 
-// variable de estado del saque
-var saque = false;
+// Estados del juego
+const ESTADO = {
+    INICIO: 0,
+    ESPERANDO: 1,
+    JUGANDO: 2,
+    MUERTE: 3
+};
+
+// Variable de estado (por defecto: INICIO)
+let estado = ESTADO.INICIO;
 
 // Constantes de los ladrillos
 const LADRILLO = {
@@ -49,132 +59,137 @@ const RAQUETA = {
 // Contador de vidas
 var vidas = 5;
 
-// funcion principal de animacion
+// funcion principal del juego
 function update() {
-    console.log("test");
-    
-    // algoritmo de animación:
-    // 1. actualizar posiciones de los elementos
-    // *rebote lados
-    if (x < 0 || x >= (canvas.width - 10)) {
-        velx = -velx;
-        s_pared.currentTime = 0;
-        s_pared.play();
-    }
-    // *rebote arriba
-    if (y < 0) {
-        vely = -vely;
-        s_pared.currentTime = 0;
-        s_pared.play();
-    }
-    //rebote en la raqueta
-    if ((x >= x1-(RAQUETA.w/2)) && (x <= x1+(RAQUETA.w/2)) && (y >= y1)) {
-        vely = -vely;
-        s_raqueta.currentTime = 0;
-        s_raqueta.play();
-    }
-    // llegada al borde de abajo
-    if (y >= (canvas.height-10)) {
-        x = canvas.width/2;
-        y = canvas.height-150;
-        vely = -vely;
-        vidas = vidas - 1;
-        saque = false;
-    }
-    // actualizar posicion
-    if (saque == true) {
-        x = x + velx;
-        y = y + vely;
-    }
-    
-    // teclas
-    window.onkeydown = (e) => {
-        // comprobar si la tecla es una que nos interese
-        if (e.key == 'ArrowRight') {
-            // raqueta a la derecha
-            x1 = x1 + velx1;
+    if (estado == ESTADO.INICIO) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.font = "25px Arial";
+        ctx.fillStyle = 'red';
+        ctx.fillText("Press START to play", 20, canvas.height/2);
+        btn_start.onclick = () => {
+            estado = ESTADO.ESPERANDO;
         }
-        if (e.key == 'ArrowLeft') {
-            // raqueta a la izquierda
-            x1 = x1 - velx1;
-        }
-        if (e.key == ' ') {
-            saque = true;
-        }
-    }
-    
-    // 2. Borrar el canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
-    // 3. dibujar elementos visibles
-    // texto marcador
-    ctx.font = "25px Arial";
-    ctx.fillStyle = 'red';
-    ctx.fillText("000", 10, 30);
-    
-    // texto vidas
-    ctx.font = "25px Arial";
-    ctx.fillStyle = 'red';
-    ctx.fillText(vidas, 340, 30);
-    
-    
-    
-    // Estructura de los ladrillos
-    const ladrillos = [];
-    
-    for (let i = 0; i < LADRILLO.F; i++) {
-        ladrillos[i] = [];
-        for (let j = 0; j < LADRILLO.C; j++) {
-            ladrillos[i][j] = {
-                x: LADRILLO.origen_x + ((LADRILLO.w + LADRILLO.padding) * j),
-                y: LADRILLO.origen_y + ((LADRILLO.h + LADRILLO.padding) * i),
-                w: LADRILLO.w,
-                h: LADRILLO.h,
-                padding: LADRILLO.padding,
-                visible: LADRILLO.visible
-            };
-        }
-    }
-    
-    // Dibujar los ladrillos
-    for (let i = 0; i < LADRILLO.F; i++) {
-        for (let j = 0; j < LADRILLO.C; j++) {
-            // Si es visible se pinta
-            if (ladrillos[i][j].visible) {
-                ctx.beginPath();
-                ctx.rect(ladrillos[i][j].x, ladrillos[i][j].y, LADRILLO.w, LADRILLO.h);
-                ctx.fillStyle = 'green';
-                ctx.fill();
-                ctx.closePath();
+    } else if (estado == ESTADO.ESPERANDO || estado == ESTADO.JUGANDO) {
+        // Borrar el canvas
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        // Dibujar elementos visibles
+        // --Texto marcador
+        ctx.font = "25px Arial";
+        ctx.fillStyle = 'red';
+        ctx.fillText("000", 10, 30);
+        // --Texto vidas
+        ctx.font = "25px Arial";
+        ctx.fillStyle = 'red';
+        ctx.fillText(vidas, 340, 30);
+        // --Estructura de los ladrillos
+        const ladrillos = [];
+        for (let i = 0; i < LADRILLO.F; i++) {
+            ladrillos[i] = [];
+            for (let j = 0; j < LADRILLO.C; j++) {
+                ladrillos[i][j] = {
+                    x: LADRILLO.origen_x + ((LADRILLO.w + LADRILLO.padding) * j),
+                    y: LADRILLO.origen_y + ((LADRILLO.h + LADRILLO.padding) * i),
+                    w: LADRILLO.w,
+                    h: LADRILLO.h,
+                    padding: LADRILLO.padding,
+                    visible: LADRILLO.visible
+                };
             }
         }
+        // --Dibujar los ladrillos
+        for (let i = 0; i < LADRILLO.F; i++) {
+            for (let j = 0; j < LADRILLO.C; j++) {
+                // Si es visible se pinta
+                if (ladrillos[i][j].visible) {
+                    ctx.beginPath();
+                    ctx.rect(ladrillos[i][j].x, ladrillos[i][j].y, LADRILLO.w, LADRILLO.h);
+                    ctx.fillStyle = 'green';
+                    ctx.fill();
+                    ctx.closePath();
+                }
+            }
+        }
+        // --Dibujar la bola
+        ctx.beginPath();
+            // bola
+            ctx.arc(x,y, 5, 0, 2*Math.PI);
+            // dibujar
+            ctx.fillStyle = 'red';
+            // rellenar
+            ctx.fill();
+        ctx.closePath();
+        // --Dibujar la raqueta
+        ctx.beginPath();
+            // rectángulo
+            ctx.rect(x1, y1, RAQUETA.w, RAQUETA.h);
+            // dibujar
+            ctx.fillStyle = 'red';
+            // rellenar
+            ctx.fill();
+        ctx.closePath();
+        if (estado == ESTADO.ESPERANDO) {
+            window.onkeydown = (e) => {
+                if (e.key == ' ') {
+                    estado = ESTADO.JUGANDO;
+                }
+            }
+        } else if (estado == ESTADO.JUGANDO) {
+            // Fisicas de la bola
+            // --Actualizar la posicion
+            x = x + velx;
+            y = y + vely;
+            // --Rebote lados
+            if (x < 0 || x >= (canvas.width - 10)) {
+                velx = -velx;
+                s_pared.currentTime = 0;
+                s_pared.play();
+            }
+            // --Rebote arriba
+            if (y < 0) {
+                vely = -vely;
+                s_pared.currentTime = 0;
+                s_pared.play();
+            }
+            // --Llegada al borde de abajo
+            if (y >= (canvas.height-10)) {
+                x = canvas.width/2;
+                y = canvas.height-150;
+                vely = -vely;
+                vidas = vidas - 1;
+                estado = ESTADO.ESPERANDO;
+                if (vidas == 0) {
+                    estado = ESTADO.MUERTE;
+                }
+            }
+            // --Rebote en la raqueta
+            if ((x >= x1-(RAQUETA.w/2)) && (x <= x1+(RAQUETA.w/2)) && (y >= y1)) {
+                vely = -vely;
+                s_raqueta.currentTime = 0;
+                s_raqueta.play();
+            }
+            // Teclas
+            window.onkeydown = (e) => {
+                // Comprobar si la tecla es una que nos interese
+                if (e.key == 'ArrowRight') {
+                    // Raqueta a la derecha
+                    x1 = x1 + velx1;
+                }
+                if (e.key == 'ArrowLeft') {
+                    // Raqueta a la izquierda
+                    x1 = x1 - velx1;
+                }
+            }
+        }
+    } else if (estado == ESTADO.MUERTE) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.font = "25px Arial";
+        ctx.fillStyle = 'red';
+        ctx.fillText("GAME OVER Press START to replay", 20, canvas.height/2);
+        btn_start.onclick = () => {
+            vidas = 5;
+            estado = ESTADO.ESPERANDO;
+        }
     }
-    
-    // Funciones para dibujar la bola
-    ctx.beginPath();
-    // bola
-    ctx.arc(x,y, 5, 0, 2*Math.PI);
-    
-    // dibujar
-    ctx.fillStyle = 'red';
-    
-    // rellenar
-    ctx.fill();
-    ctx.closePath();
-    
-    // Funciones para dibujar la raqueta
-    ctx.beginPath();
-    // rectángulo
-    ctx.rect(x1, y1, RAQUETA.w, RAQUETA.h);
-    
-    // dibujar
-    ctx.fillStyle = 'red';
-    
-    // rellenar
-    ctx.fill();
-    ctx.closePath();
-    
-    // 4. volver a ejecutar update cuando toque
     requestAnimationFrame(update);
 }
 
