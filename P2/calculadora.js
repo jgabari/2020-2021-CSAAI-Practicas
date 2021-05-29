9993d654c1d96624263e1be1d94e3f745c9a938a
jgabari <javigabari@gmail.com>
2021-05-29 18:39:51 +0200

    ejercicio4

--- P2/calculadora.js
+++ P2/calculadora.js
@@ -1,7 +1,13 @@
-const btn = document.getElementById("btn");
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
 const display = document.getElementById("display");
 
-btn.onclick = () => {
-    console.log("Click");
-    display.innerHTML = "Click";
-  }
+btn1.onclick = () => {
+    console.log("1");
+    display.innerHTML += "1";
+}
+
+btn2.onclick = () => {
+    console.log("2");
+    display.innerHTML += "2";
+}
